af32e064ba4ca13876af67e5feab8828f7ef1a70
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-19 09:58:34 +0100

    Update detection.service.ts

--- src/app/services/detection.service.ts
+++ src/app/services/detection.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetectionService {
 
-  constructor() { }
+  constructor(
+    private firestore: AngularFirestore
+  ) { }
+
+
+  create_NewDetection(record) {
+    return this.firestore.collection('Detections').add(record);
+  }
+
+  read_Detections() {
+    return this.firestore.collection('Detections').snapshotChanges();
+  }
+
+  update_Detection(recordID, record) {
+    this.firestore.doc('Detections/' + recordID).update(record);
+    console.log('updated');
+  }
+
+  delete_Detection(record_id) {
+    this.firestore.doc('Detections/' + record_id).delete();
+  }
 }
