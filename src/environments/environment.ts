2e7325bc00fa2f70ee06404390c764524931181a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-19 09:22:11 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyAU2-_9axlMtx7rOE94MIdZYZ5FDeF314s",
+    authDomain: "insectes-bca64.firebaseapp.com",
+    projectId: "insectes-bca64",
+    storageBucket: "insectes-bca64.appspot.com",
+    messagingSenderId: "578411072107",
+    appId: "1:578411072107:web:0d55fc5e213144da85bf2e",
+    measurementId: "G-603RP901BL"
+  
 };
