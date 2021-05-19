5b2680da54a8c26f74467e581b643900c0734ce9
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-19 09:58:47 +0100

    Update information.service.ts

--- src/app/services/information.service.ts
+++ src/app/services/information.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InformationService {
 
-  constructor() { }
+  constructor(
+    private firestore: AngularFirestore
+  ) { }
+
+
+  create_NewInformation(record) {
+    return this.firestore.collection('Informations').add(record);
+  }
+
+  read_Informations() {
+    return this.firestore.collection('Informations').snapshotChanges();
+  }
+
+  update_Information(recordID, record) {
+    this.firestore.doc('Informations/' + recordID).update(record);
+    console.log('updated');
+  }
+
+  delete_Information(record_id) {
+    this.firestore.doc('Informations/' + record_id).delete();
+  }
 }
